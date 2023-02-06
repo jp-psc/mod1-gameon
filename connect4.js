1b22e5862144c595c82c791cae75be7101b4808a
jpxyzog <119944675+jpxyzog@users.noreply.github.com>
2023-02-05 23:31:51 -0500

    some updates

--- connect4.js
+++ connect4.js
@@ -134,10 +134,3 @@ let outro = document.getElementById("outro");
                 audio.play();
         }
         outro.addEventListener("click", playOutro);
-
-let csup = document.getElementById("csup");
-        function playCsup() {
-            let audio = new Audio("csup.mp3");
-                audio.play();
-        }
-        csup.addEventListener("")
